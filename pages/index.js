3a499c2e4eb2cda209708618617e7b051b12bf0a
Will Koury <wkoury@hey.com>
2020-10-19 11:44:46 -0500

    Implements new color scheme

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>Oklahama Early Fall Tournament</title>
       </Head>
       <main>
-        <p>The Oklahoma Academic Team</p>
+        <p>The OU Academic Team</p>
         <p>is hosting</p>
         <span>an <a href="https://www.naqt.com/registration/field.jsp?tournament_id=11791" rel="noopener noreferrer" target="_blank">online NAQT tournament</a></span>
         <p>on <a href="https://www.notion.so/Oklahoma-Early-Fall-Tournament-2020-d3e32cef3be74fa99dd45914002474a3" rel="noopener noreferrer" target="_blank">October 24</a>!</p>
