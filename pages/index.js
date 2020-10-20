946b3e89eaaf094d0f9dc6d09a43c141d67c0159
Will Koury <wkoury@hey.com>
2020-10-20 08:25:27 -0500

    Renames schedule to tournament HQ

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         </div>
         <br></br>
         <h3>Important Links:</h3>
-        <p><a href="https://www.notion.so/c30c250946a04b469ef752d5b4ceb796?v=9d216d9730344d80bfc5d197bffb25c7" rel="noopener noreferrer" target="_blank">Schedule</a></p>
+        <p><a href="https://www.notion.so/c30c250946a04b469ef752d5b4ceb796?v=9d216d9730344d80bfc5d197bffb25c7" rel="noopener noreferrer" target="_blank">Tournament HQ</a></p>
       </main>
     </div>
   );
