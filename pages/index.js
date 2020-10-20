4e3c30f2b112d68156d912c48f964aa71cdfc4c7
Will Koury <wkoury@hey.com>
2020-10-20 08:11:13 -0500

    Adds schedule as separate document

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,15 @@ export default function Home() {
         <title>Oklahama Early Fall Tournament</title>
       </Head>
       <main>
-        <p>The OU Academic Team</p>
-        <p>is hosting</p>
-        <span>an <a href="https://www.naqt.com/registration/field.jsp?tournament_id=11791" rel="noopener noreferrer" target="_blank">online NAQT tournament</a></span>
-        <p>on <a href="https://www.notion.so/Oklahoma-Early-Fall-Tournament-2020-d3e32cef3be74fa99dd45914002474a3" rel="noopener noreferrer" target="_blank">October 24</a>!</p>
+        <div className={styles.announcement}>
+          <p>The OU Academic Team</p>
+          <p>is hosting</p>
+          <span>an <a href="https://www.naqt.com/registration/field.jsp?tournament_id=11791" rel="noopener noreferrer" target="_blank">online NAQT tournament</a></span>
+          <p>on <a href="https://www.notion.so/Oklahoma-Early-Fall-Tournament-2020-d3e32cef3be74fa99dd45914002474a3" rel="noopener noreferrer" target="_blank">October 24</a>!</p>
+        </div>
+        <br></br>
+        <h3>Important Links:</h3>
+        <p><a href="https://www.notion.so/c30c250946a04b469ef752d5b4ceb796?v=9d216d9730344d80bfc5d197bffb25c7" rel="noopener noreferrer" target="_blank">Schedule</a></p>
       </main>
     </div>
   );
