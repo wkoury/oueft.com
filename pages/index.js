4ef40cff31aae77432176effc538e1065d46c1e3
Will Koury <wkoury@hey.com>
2020-10-29 14:11:50 -0500

    Leaves only results link for privacy

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,9 @@ export default function Home() {
         <div className={styles.announcement}>
           <p>The OU Academic Team</p>
           <p>hosted</p>
-          <span>an <a href="https://www.naqt.com/registration/field.jsp?tournament_id=11791" rel="noopener noreferrer" target="_blank">online NAQT tournament</a></span>
-          <p>on <a href="https://www.notion.so/Oklahoma-Early-Fall-Tournament-2020-d3e32cef3be74fa99dd45914002474a3" rel="noopener noreferrer" target="_blank">October 24</a>!</p>
+          <span>an <a href="https://www.naqt.com/stats/tournament/standings.jsp?tournament_id=11791" rel="noopener noreferrer" target="_blank">online NAQT tournament</a></span>
+          <p>on October 24!</p>
         </div>
-        <br></br>
-        <Link href="https://www.naqt.com/stats/tournament/standings.jsp?tournament_id=11791"><a rel="noopener noreferrer" target="_blank">Results</a></Link>
       </main>
       <div className={styles.footer}>
         <span>©2020 <a href="https://wkoury.com" rel="noopener noreferrer" target="_blank">Will Koury</a></span>
