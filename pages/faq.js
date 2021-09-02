91ee1280aac666a7e4cb49ca94169f2dcae4cd53
Will Koury <wkoury@hey.com>
2021-09-01 19:37:35 -0500

    Update FAQ

--- pages/faq.js
+++ pages/faq.js
@@ -19,6 +19,11 @@ export default function Faq() {
         question='What is the fee structure for this year?'
         answer='Fees are as follows: $80 for a first team, $65 for all following teams, and a $10 discount per buzzer, as well as a $10 per team distance discount. The distance discount will only apply if you live more than 100 miles from the University of Oklahoma.'
       />
+
+      <Question
+        question='What will the field size be?'
+        answer='At the moment, the field is capped at 16 teams. If we can get the resources required to accomodate more teams, we intend to do so, in which case those teams on the waitlist will be let in.'
+      />
       <Footer />
     </div>
   );
