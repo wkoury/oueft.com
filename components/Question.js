7fa22ecf0533e4a72d9289e25f0df6c92bc32700
Will Koury <wkoury@hey.com>
2021-09-07 08:20:37 -0500

    Better formatting for FAQ

--- components/Question.js
+++ components/Question.js
@@ -4,8 +4,8 @@ import styles from '../styles/Home.module.css';
 const Question = props => {
       return(
 	    <React.Fragment>
-	    <div className={styles.faqQuestion}>{props.question}</div>
-		  <p>{props.answer}</p>
+		  <div className={styles.faqQuestion}>{props.question}</div>
+		  <p style={{fontSize: '0.9rem', width: '50%'}}>{props.answer}</p>
 	    </React.Fragment>
       )
 
