e37d2ac031e465e6fe3e53446bbb9751419b3a88
Will Koury <wkoury@hey.com>
2021-10-01 20:29:55 -0500

    Remove back link
    Because we had to change how the footer goes onto the page

--- pages/stats/index.js
+++ pages/stats/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import React from "react";
 import styles from "../../styles/Home.module.css";
-import Link from "next/link";
 
 export default function Index () {
     return(
@@ -16,9 +15,6 @@ export default function Index () {
                     src="stats/oueft_standings.html"
                     name="Stats"
                 ></iframe>
-            </div>
-            <div className={styles.footer}>
-                <Link href="/">Back</Link>
             </div>
         </div>
     );
